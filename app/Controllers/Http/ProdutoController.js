9b3ccb080a740abed3f00b256eaeb13fb4a63895
Wisley Rodrigues <37223066+WisleyRodrigues@users.noreply.github.com>
2020-06-12 23:38:18 -0400

    adição de banco de dados e migration de produtos

---
+++ app/Controllers/Http/ProdutoController.js
@@ -0,0 +1,69 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with produtos
+ */
+class ProdutoController {
+  /**
+   * Show a list of all produtos.
+   * GET produtos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new produto.
+   * POST produtos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single produto.
+   * GET produtos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update produto details.
+   * PUT or PATCH produtos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a produto with id.
+   * DELETE produtos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ProdutoController
---
+++ database/migrations/1592018404759_produto_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProdutoSchema extends Schema {
+  up () {
+    this.create('produtos', (table) => {
+      table.increments()
+      table.string('descricao')
+      table.string('nome')
+      table.integer('qtd')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('produtos')
+  }
+}
+
+module.exports = ProdutoSchema
